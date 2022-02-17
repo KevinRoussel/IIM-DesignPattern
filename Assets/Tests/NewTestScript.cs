0ecba13dfd98b77d6e3b88e897aeb1e026b724f1
Red <lebruitdered@gmail.com>
2022-02-17 08:58:35 +0100

    Arrange first Unit Test

--- Assets/Tests/NewTestScript.cs
+++ Assets/Tests/NewTestScript.cs
@@ -1,26 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-public class NewTestScript
+public class HealthTests
 {
+    Health CreateHealth(int maxHealth, int startHealth) 
+        => new GameObject("Health GameObject")
+            .AddComponent<Health>()
+            .SetField("_maxHealth", 10)
+            .SetField("_startHealth", 10)
+            .CallAwake();
+
     // A Test behaves as an ordinary method
     [Test]
-    public void NewTestScriptSimplePasses()
+    public void HealthStartsWith10AndDamage9Obtain1()
     {
         // Arrange
-        var h = new GameObject("test").AddComponent<Health>();
+        var h = CreateHealth(10, 10);
 
         // Act
-        h.TakeDamage(100);
+        h.TakeDamage(9);
 
         // Assert
-        Assert.AreEqual(0, h.CurrentHealth);
-
+        Assert.AreEqual(1, h.CurrentHealth);
     }
 
+
+
+
+
+    #region osef
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
     // `yield return null;` to skip a frame.
     [UnityTest]
@@ -30,4 +42,5 @@ public class NewTestScript
         // Use yield to skip a frame.
         yield return null;
     }
+    #endregion
 }
